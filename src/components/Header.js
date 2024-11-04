7b6d894c8aa8968b9bd0e9129ea291ef8b55fb1b
youminki <dbalsrl7648@gmail.com>
2024-11-05 00:35:36 +0900

    fix : Header 위치 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,22 +26,24 @@ const HeaderContainer = styled.header`
   justify-content: space-between;
   align-items: center;
   width: 100%;
+  max-width: 600px; // Adjust max-width to match the content
+  left: 50%;
+  transform: translateX(-50%); // Center the header
   z-index: 10;
-  max-width: 600px;
-  margin-top: 41px;
+  margin-top: 41px; // Adjust margin if necessary
 `;
 
 const Logo = styled.img`
   width: 83px;
   height: 35px;
-  margin-left: 29px;
+  margin-left: 29px; // Optional, adjust as needed
 `;
 
 const RegisterButton = styled.button`
   padding: 10px 15px;
   background-color: ${({ theme }) => theme.colors.white};
   border-radius: 20px;
-  margin-right: 29px;
+  margin-right: 29px; // Optional, adjust as needed
   cursor: pointer;
   font-family: 'NanumSquare Neo OTF';
   font-weight: 700;
