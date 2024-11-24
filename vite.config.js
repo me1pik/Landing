9fbf6d583e741453ce14144b222deeb9663e8d05
youminki <dbalsrl7648@gmail.com>
2024-11-24 19:18:07 +0900

    fix : redirects 추가

--- vite.config.js
+++ vite.config.js
@@ -8,22 +8,16 @@ const isNetlify = process.env.NETLIFY === 'true'; // Netlify 환경 체크
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: isNetlify
-      ? undefined
-      : {
+    https: !isNetlify // Netlify 환경에서는 https 비활성화
+      ? {
           key: fs.readFileSync(
-            path.resolve(
-              new URL(import.meta.url).pathname,
-              '../cert/localhost.com-key.pem'
-            )
+            path.resolve(__dirname, 'cert/localhost.com-key.pem') // 로컬 환경에서의 경로
           ),
           cert: fs.readFileSync(
-            path.resolve(
-              new URL(import.meta.url).pathname,
-              '../cert/localhost.com.pem'
-            )
+            path.resolve(__dirname, 'cert/localhost.com.pem') // 로컬 환경에서의 경로
           ),
-        },
-    open: true,
+        }
+      : undefined, // Netlify 환경에서는 https 설정을 하지 않음
+    open: true, // 서버 시작 시 브라우저 자동 열기
   },
 });
